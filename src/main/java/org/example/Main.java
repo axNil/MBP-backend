64be827cfaad9393175313e56c86338716909bf7
Axel nilsson <axel.nilsson01@gmail.com>
2022-12-07 12:36:32 +0100

    st

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,15 @@
 package org.example;
 
+import io.javalin.Javalin;
+
 public class Main {
     public static void main(String[] args) {
-
+        Javalin app = Javalin.create(config -> {
+            config.plugins.enableCors(cors -> {
+                cors.add(it -> {
+                    it.anyHost();
+                });
+            });
+        }).start(5008);
     }
 }
