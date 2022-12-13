4bea4eb91b759b9d078097b7b6830bab42cd2f25
Axel nilsson <axel.nilsson01@gmail.com>
2022-12-13 13:09:59 +0100

    Update APIRunner.java

--- src/main/java/org/example/APIRunner.java
+++ src/main/java/org/example/APIRunner.java
@@ -7,7 +7,6 @@ import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.JsonNode;
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
-import org.json.JSONObject;
 
 import java.io.IOException;
 import java.util.HashMap;
